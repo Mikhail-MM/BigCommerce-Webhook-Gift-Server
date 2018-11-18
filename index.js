deca2058ace17baec5e3ab97e6f5c2357b0b475f
Mikhail Maslyuk <leakymee@bu.edu>
2018-11-18 14:49:21 -0500

    Add start script for Heroku

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const rp = require('request-promise')
+const rp = require('request-promise');
 
-const app = express()
+const app = express();
+
+const specialGift = {
+
+};
 
 app.post('/webhooks', (req, res, next) => {
-	console.log("Hook Received.")
-	console.log(req.body)
+	console.log("Hook Received.");
+	console.log(req.body);
 });
 
 app.get('*', (req, res) => {
-	console.log("Catch-All Handler")
-	res.status(200).send("All Clear Chief.")
+	console.log("Catch-All Handler");
+	res.status(200).send("All Clear Chief.");
 });
 
 const port = process.env.PORT || 3000;
