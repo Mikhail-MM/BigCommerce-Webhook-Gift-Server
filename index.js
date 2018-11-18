790124c9461169b9e19da92f3ccfa01011c91a2f
Mikhail Maslyuk <leakymee@bu.edu>
2018-11-18 14:46:29 -0500

    Initial Commit.

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const rp = require('request-promise')
+
+const app = express()
+
+app.post('/webhooks', (req, res, next) => {
+	console.log("Hook Received.")
+	console.log(req.body)
+});
+
+app.get('*', (req, res) => {
+	console.log("Catch-All Handler")
+	res.status(200).send("All Clear Chief.")
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+
+console.log(`Webhook Gift Server listening on ${port}`);
