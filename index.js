7291d7c03ce99dcaaf6512bac1e27edfed398555
Mikhail Maslyuk <leakymee@bu.edu>
2018-11-18 17:38:23 -0500

    Analyze request

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ const specialGift = {
 
 };
 
+const initailizeHooks = () => {
+
+}
+
 app.post('/webhooks', (req, res, next) => {
 	console.log("Hook Received.");
 	console.log(req.body);
+	console.log(req)
+	res.end()
 });
 
 app.get('*', (req, res) => {
@@ -22,3 +28,30 @@ const port = process.env.PORT || 3000;
 app.listen(port);
 
 console.log(`Webhook Gift Server listening on ${port}`);
+
+
+/*
+
+Hook Initializer Request
+
+{
+ "scope": "store/cart/lineItem/*",
+ "destination": "https://bigcommerce-webhooks.herokuapp.com/webhooks",
+ "is_active": true
+}
+
+Hook Initializer Response
+
+{
+    "id": 15353127,
+    "client_id": "1uxt9bl7pozqbdhsswyt0bt7h6q0fo2",
+    "store_hash": "h3sfhsws7q",
+    "scope": "store/cart/lineItem/*",
+    "destination": "https://bigcommerce-webhooks.herokuapp.com/webhooks",
+    "headers": null,
+    "is_active": true,
+    "created_at": 1542580553,
+    "updated_at": 1542580553
+}
+
+*/
